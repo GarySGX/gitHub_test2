d3266ccd1e0476ecdefaa1c2803852df57ebae41
GarySGX <shenguangxu1009@gmail.com>
2020-05-19 15:44:21 +0800

    Decorator1
    Trial 1.1

---
+++ Decorator1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 10:09:27 2020
+
+@author: GarySGX
+"""
+
+def logged(func):
+    def with_logging(*args, **kwargs):
+        print(func.__name__ + " was called")
+        return func(*args, **kwargs)
+    return with_logging
+
+@logged
+def f(x):
+   """does some math"""
+   return x + x * x
+
+f = logged(f)
+
+print(f.__name__)
+
+
+print("docstring starts")
+print(f.__doc__)
